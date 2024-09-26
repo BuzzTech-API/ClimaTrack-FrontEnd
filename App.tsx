364583be4f8616bb644688eadab2a8d01aa0b7a6
lima2206 <vitor.lima2206@gmail.com>
2024-09-26 19:32:57 -0300

    feat: navegação entre a tela de pesquisa e tela teste
    Futuramente a tela teste será a tela onde será feita a requisição pro backend
    Esse commit traz as informações da tela de pesquisa para a tela de teste

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
+import React from 'react';
 import { SafeAreaView, StatusBar, Text, TextInput, View } from 'react-native';
-import SeachScreen from '~/screens/SearchLocation';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import SearchLocation from '~/screens/SearchLocation';
+import TesteResult from '~/screens/TesteResultScreen';
 
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   return (
-    <View>
-      <SeachScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='search'>
+        <Stack.Screen name="search" component={SearchLocation}/>
+        {/* esse erro é estranho mas ele continua funcionando normal, olhe e ignore */}
+        <Stack.Screen name="result" component={TesteResult}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }

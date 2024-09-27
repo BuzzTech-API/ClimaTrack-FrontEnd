6504b8b45740ff2dcbed52364c03a476088f1632
Natanael <natanaelsm0102@gmail.com>
2024-09-27 16:14:41 -0300

    fix: Função de pegar dados de pluiviosidade e temperatura funcionando.

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Arquivo para definir as configurações das requisições como url, headers e etc..
 
-const baseURL = "https://localhost:5000"
+const baseURL = "http://10.0.2.2:8000";
 
 const api = axios.create({
     baseURL,
--- src/api/getPluvTemp.ts
+++ src/api/getPluvTemp.ts
@@ -1,15 +1,16 @@
-import api from "./api";
-import {reqPluvTempType} from "../types/reqPluvTempType";
+// fetchPluviTemp.js
+import api from './api';
+import { reqPluvTempType } from '../types/reqPluvTempType';
 
-// Código para pegar os dados de pluviosidade e temperatura do backend.
+export const fetchPluviTemp = async (infosReq: reqPluvTempType) => {
 
-export const fetchPluviTemp = async(infosReq: reqPluvTempType) => {
-    await api.get("/getPluviTemp",{
-        params: infosReq
-    } )
-    .then(response => {
-        return response.data;
-    }).catch(error => {
-        throw new Error(`Erro ao pegar os dados de pluviosidade e temperatura: ${error.message}`)
-    })
-}
+  const parameters = `?parameters=T2M&longitude=${infosReq.longitude}&latitude=${infosReq.latitude}&start=${infosReq.startDate}&end=${infosReq.endDate}`;
+
+  try {
+    const response = await api.get(`/climate/${parameters}`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar dados climáticos:", error);
+    throw new Error('Erro ao buscar dados climáticos');
+  }
+};

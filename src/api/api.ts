142acd8141fd06c672b69a976f7e56fbd33a38f2
Natanael <natanaelsm0102@gmail.com>
2024-10-19 11:03:46 -0300

    fix: retorno do id na rota de adicionar locallização.

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,8 +5,7 @@ import axios from 'axios';
 const baseURL = 'http://10.0.2.2:8000';
 
 const api = axios.create({
-  baseURL,
-  // headers: {}
+  baseURL
 });
 
 export default api;
---
+++ src/components/ConfirmDelLocation.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Modal } from 'react-native';
+import { delLocation } from '~/api/deleteLocation';
+
+interface ConfirmDelLocationProps {
+  modalVisible: boolean;
+  onCancel: () => void;
+  idLocation: string;
+}
+
+export const ConfirmDelLocation: React.FC<ConfirmDelLocationProps> = ({ modalVisible, onCancel, idLocation }) => {
+
+  const handleDelete = async () => {
+    try {
+      const res = await delLocation(idLocation);
+
+      if (res.sucesss) {
+        alert("Localização deletada com sucesso!");
+        onCancel();
+      } else {
+        alert("Erro ao excluir localização: " + res.status);
+      }
+    } catch (error) {
+      alert("Erro inesperado ao tentar excluir a localização: " + error);
+    }
+  };
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={onCancel}
+    >
+      <View style={styles.centralizerContainer}>
+        <View style={styles.mainContainer}>
+          <View style={styles.containerText}>
+            <Text style={styles.text}>Realmente deseja excluir a área salva?</Text>
+          </View>
+          <View style={styles.containerButtons}>
+            <TouchableOpacity style={styles.buttonLeft} onPress={handleDelete}>
+              <Text style={styles.textButton}>Sim</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonRigth} onPress={onCancel}>
+              <Text style={styles.textButton}>Não</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centralizerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  mainContainer: {
+    backgroundColor: '#F1F3F0',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  containerText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 25,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  containerButtons: {
+    flexDirection: 'row',
+  },
+  buttonLeft: {
+    width: '50%',
+    borderBottomStartRadius: 10,
+    backgroundColor: '#53B853',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+  },
+  buttonRigth: {
+    width: '50%',
+    borderBottomEndRadius: 10,
+    backgroundColor: '#D10000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+  },
+  textButton: {
+    color: '#FFFFFF',
+  },
+});
